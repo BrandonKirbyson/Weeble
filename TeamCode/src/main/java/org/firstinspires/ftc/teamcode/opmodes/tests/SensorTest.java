1086334a75bb375f8be831d15b8f1c7ffb02b519
BrandonKirbyson <brandonkirbyson@gmail.com>
2024-05-29 09:52:10 -0700

    Tested distance sensor

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/SensorTest.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.opmodes.tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+@TeleOp(name = "SensorTest", group = "tests")
+public class SensorTest extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        DistanceSensor front = hardwareMap.get(DistanceSensor.class, "frontDist");
+
+        waitForStart();
+
+        while (opModeIsActive() && !isStopRequested()) {
+            telemetry.addData("Dist", front.getDistance(DistanceUnit.INCH));
+            telemetry.update();
+        }
+    }
+}
