236071f5db78252e6846844f58a47d7ca6898d49
BrandonKirbyson <brandonkirbyson@gmail.com>
2024-06-05 12:22:28 -0700

    Added vision portal init

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/vision/Vision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/vision/Vision.java
@@ -1,16 +1,26 @@
 package org.firstinspires.ftc.teamcode.util.vision;
 
+import android.util.Size;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.util.vision.sensors.SensorManager;
+import org.firstinspires.ftc.vision.VisionPortal;
 
 public class Vision {
     private final WebcamName webcam;
     private final SensorManager sensorMapping;
 
+    private final VisionPortal visionPortal;
+
     public Vision(HardwareMap hardwareMap) {
         this.webcam = hardwareMap.get(WebcamName.class, "webcam");
         sensorMapping = new SensorManager(hardwareMap);
+
+        visionPortal = new VisionPortal.Builder()
+                .setCamera(webcam)
+                .setStreamFormat(VisionPortal.StreamFormat.MJPEG)
+                .setCameraResolution(new Size(1280, 720))
+                .build();
     }
 
     public void update() {
