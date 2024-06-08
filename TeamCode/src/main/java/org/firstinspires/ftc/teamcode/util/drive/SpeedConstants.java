352d2a2d8eae299047c5e451ba6d7867440133dd
BrandonKirbyson <brandonkirbyson@gmail.com>
2024-06-08 12:40:52 -0700

    Proper speeds

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/drive/SpeedConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/drive/SpeedConstants.java
@@ -4,6 +4,6 @@ import com.acmerobotics.dashboard.config.Config;
 
 @Config
 public class SpeedConstants {
-    public static double ManualDrive = 20;
-    public static double ManualTurn = 0.5;
+    public static double ManualDrive = 0.4;
+    public static double ManualTurn = 0.3;
 }
