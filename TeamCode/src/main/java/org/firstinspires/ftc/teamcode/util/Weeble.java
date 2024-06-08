f35e645af520b7630fd37b565c0fcb8e37563813
BrandonKirbyson <brandonkirbyson@gmail.com>
2024-06-08 12:41:04 -0700

    Added safety for falling

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Weeble.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Weeble.java
@@ -3,6 +3,9 @@ package org.firstinspires.ftc.teamcode.util;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.IMU;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+import org.firstinspires.ftc.teamcode.util.arms.ArmPosition;
+import org.firstinspires.ftc.teamcode.util.arms.Arms;
+import org.firstinspires.ftc.teamcode.util.drive.DriveState;
 import org.firstinspires.ftc.teamcode.util.drive.GyroDrive;
 import org.firstinspires.ftc.teamcode.util.head.Head;
 import org.firstinspires.ftc.teamcode.util.vision.Vision;
@@ -10,7 +13,7 @@ import org.firstinspires.ftc.teamcode.util.vision.Vision;
 public class Weeble {
     public final Vision vision;
     public final GyroDrive drive;
-    //    public final Arms arms;
+    public final Arms arms;
     public final Head head;
 
     private final IMU imu;
@@ -18,7 +21,7 @@ public class Weeble {
     public Weeble(HardwareMap hardwareMap) {
         vision = new Vision(hardwareMap);
         drive = new GyroDrive(hardwareMap);
-//        arms = new Arms(hardwareMap);
+        arms = new Arms(hardwareMap);
         head = new Head(hardwareMap);
 
         imu = hardwareMap.get(IMU.class, "imu");
@@ -28,14 +31,18 @@ public class Weeble {
     public void update() {
         YawPitchRollAngles angles = imu.getRobotYawPitchRollAngles();
         drive.update(angles);
+        head.updateAngles(angles);
+
         vision.update();
 
         head.update(vision.getTrackingCenter());
 
-//        if (drive.getLastState() == DriveState.DRIVING && drive.getState() == DriveState.STOPPED) {
-//            arms.setArmPosition(ArmPosition.Forward);
-//        } else if (drive.getLastState() == DriveState.STOPPED && drive.getState() == DriveState.DRIVING) {
-//            arms.setArmPosition(ArmPosition.Down);
-//        }
+        if (drive.getLastState() != DriveState.STOPPED && drive.getState() == DriveState.STOPPED) {
+            arms.setArmPosition(ArmPosition.Forward);
+            head.protectionMode();
+        } else if (drive.getLastState() == DriveState.STOPPED && drive.getState() != DriveState.STOPPED) {
+            arms.setArmPosition(ArmPosition.Down);
+            head.reset();
+        }
     }
 }
